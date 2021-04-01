465cbeadf14d46cde78f1d3de8edc7fb3e100505
wangjie <1540302851@qq.com>
2021-04-02 01:41:03 +0800

    refeat 完成上报基本核心逻辑，完成js异常和静态资源异常上报基本逻辑

--- report-core.js
+++ report-core.js
@@ -1,63 +1,142 @@
+import axios from 'axios'
+export const install = function(Vue){
+    let events = [
+        'open', 'send',
+        'loadstart', 'loadend', 'load',
+        'abort'
+    ];
+    let oldXMLHttpRequest = window.XMLHttpRequest;
+    reset()
 
-export function install(Vue) {
-    let oldErrorHandler = Vue.config.errorHandler;
-    if (typeof oldErrorHandler === 'function') {
-        function handlerError(error, vm, info){
-            console.log('errorHandler',error, 123,vm, 456,info)
-            // do something to report
-            oldErrorHandler.call(Vue, error,vm,info)
-        }
-        Vue.config.errorHandler = handlerError
-    }
     
-
-    (function(){
-        function ajaxEventTrigger(event) {
-            var ajaxEvent = new CustomEvent(event, {detail: this})
-            window.dispatchEvent(ajaxEvent)
+        const report = {
+            send(err){
+                let url = "http://localhost:3000/create"
+                axios.post(url, {detail: JSON.stringify(err)})
+            }
+        }
+        const shared = {
+            noop(){}
         }
 
-        var oldXMLHttpRequest = window.XMLHttpRequest;
+        /**vue js runtime error */
+        function overVueErrorHandler(){
+            let origal = Vue.config.errorHandler || shared.noop;
+            return function(err,vm,info){
+                report.send({
+                    message: err.message,
+                    stack: err.stack,
+                    info,
+                    errorType: 'referenceError',
+                    type: err.srcElement.localName
+                })
+                origal.call(this,err, info, vm)
+            }
+
+        }
+        
+        Vue.config.errorHandler = overVueErrorHandler()
+        
+
+        /**vue resource load error */
+        function matchResourceError(srcElement){
+            let matchs = [HTMLImageElement, HTMLLinkElement, HTMLScriptElement]
+            return matchs.filter(proto => srcElement instanceof proto).length
+        }
+        function getResourceError(err){
+            if(matchResourceError(err.srcElement)){
+                report.send({
+                    message: err.srcElement.currentSrc,
+                    site: err.srcElement.baseURI,
+                    errorType: 'resourceError',
+                    type: err.srcElement.localName
+                })
+            }
+        }
+        window.addEventListener('error', getResourceError, true)
+        
+
+        /**promise not catch error */
+        window.addEventListener('unhandledrejection', e => {
+            // report.send(e)
+            console.error('PROMISE', e)
+            console.error(`UNHANDLED PROMISE REJECTION: ${e.reason}`);
+        }, false)
+
+
+        /**ajax interactive exception */
+        function createCustomEvent(eventName){
+            let event = new CustomEvent(eventName, { detail: this })
+            window.dispatchEvent(event)
+        }
+        
+        
         function newXHR(){
-            var realXHR = new oldXMLHttpRequest()
-            var events = [
-                'abort','error','load','loadstart',
-                'loadend','progress','timeout','readystatechange',
-            ]
-            events.forEach(event => {
-                realXHR.addEventListener(event, function(){
-                    ajaxEventTrigger.call(this, `ajax${event[0].toUpperCase() + event.slice(1)}`)
-                },false)
-            })
+            let realXHR = new oldXMLHttpRequest()
+                let eventName
+                events.forEach(event => {
+                    realXHR.addEventListener(event, function(){
+                        eventName = `ajax${event[0].toUpperCase() + event.slice(1)}`
+                        createCustomEvent(eventName)
+                    })
+                })
+            
             return realXHR
         }
-        window.XMLHttpRequest = newXHR;
-
-
-        // 如何调用？
-        var xhr = new XMLHttpRequest();
-        var events = [
-            'abort','error','load','loadstart',
-            'loadend','progress','timeout','readystatechange',
-        ]
-        events.map(event => {
+        events.forEach(event => {
             window.addEventListener(`ajax${event[0].toUpperCase() + event.slice(1)}`, e => {
-                // console.log('e.detail.responseURL', e.detail.responseURL)
-                // if(e.detail.responseURL.indexOf('baidu.com') !== -1) {
-                    console.log(`ajax${event[0].toUpperCase() + event.slice(1)}`, e,e.detail)
-                // }
-                
+                ajaxHandlerCb(event, {_reportEvent: true, e})
             })
         })
-        xhr.open('get', 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=xhr%20state%20canceled&fenlei=256&oq=xhr.open%25E5%258F%2582%25E6%2595%25B0&rsv_pq=aea827e20000ab1c&rsv_t=20cbakG5E7IreDtitzEBPm%2BNcgy5liSfRCruTv%2FHO2MoKpdGi%2BskDu5ViaI&rqlang=cn&rsv_enter=1&rsv_dl=tb&rsv_btype=t&inputT=8380&rsv_sug3=43&rsv_sug1=27&rsv_sug7=101&rsv_sug2=0&rsv_sug4=8494')
-        xhr.send()
-        
-        xhr.open('get', 'https://ips-gateway-sit.qizhidao.com/common/currency/list/page/1/10')
-        xhr.send()
+        window.XMLHttpRequest = newXHR
+         
+        const ajaxReportHandler = {
+            open(e){
+                // console.log('open', e)
+                // report.send(err)
+            },
+            send(e){
+                // console.log('send', e)
+            },
+            loadstart(e){
+                // console.log('loadstart', e)
+            },
+            loadend(e){
+                // console.log('loadend', e)
+            },
+            load(e){
+                // console.log('load', e)
+            },
+            abort(e){
+                // console.log('abort', e)
+            },
+        }
+        function ajaxHandlerCb(eventName, e) {
+            // eventName = eventName.slice(4).toLowerCase()
+            // console.log('自定义类型？',e)
+            ajaxReportHandler[eventName](e)
+        }
 
-        window.addEventListener("unhandledrejection", event => {
-            console.warn(`UNHANDLED PROMISE REJECTION: ${event.reason}`);
-            console.log(event.reason.stack)
-        });
-    })()
+
+        /**项目重新刷新，是否又会重新引入一次？再次绑定再次初始化？ */
+        function reset(){
+            removeAjaxEvent()
+            removeResourceEvent()
+            removePromiseEvent()
+        }
+        function removeAjaxEvent() {
+            let realXHR = new oldXMLHttpRequest()
+            events.forEach(event => {
+                realXHR.removeEventListener(event, () => shared.noop)
+                window.removeEventListener( `ajax${event[0].toUpperCase() + event.slice(1)}`, () => shared.noop)
+            })
+        }
+        function removeResourceEvent(){
+            window.removeEventListener('error', () => shared.noop)
+        }
+        function removePromiseEvent(){
+            window.removeEventListener('unhandledrejection', () => shared.noop)
+        }
+        
+    
 }
