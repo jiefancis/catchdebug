264867bac0c16b126e3eba3fef3bd890c8d2fc7c
wangjie <1540302851@qq.com>
2021-04-02 11:16:52 +0800

    feat 简易版异常监控原理

--- report-core.js
+++ report-core.js
@@ -1,5 +1,6 @@
+import Vue from 'vue'
 import axios from 'axios'
-export const install = function(Vue){
+// export const install = function(Vue){
     let events = [
         'open', 'send',
         'loadstart', 'loadend', 'load',
@@ -7,23 +8,39 @@ export const install = function(Vue){
     ];
     let oldXMLHttpRequest = window.XMLHttpRequest;
     reset()
+    let options = {
+        url: "http://localhost:3000/create",
+
+    }
 
     
         const report = {
             send(err){
-                let url = "http://localhost:3000/create"
-                axios.post(url, {detail: JSON.stringify(err)})
+                // let url = "http://localhost:3000/create"
+                axios.post(options.url, {detail: JSON.stringify(err)})
             }
         }
         const shared = {
             noop(){}
         }
 
+        const captureHandler = {
+            init,
+            captureVueError,
+            captureResourceError,
+            capturePromiseError,
+            captureAjaxError
+        }
+
+        function init(_options){
+            Object.assign(options, _options)
+            captureHandler.captureAjaxError()
+        }
         /**vue js runtime error */
-        function overVueErrorHandler(){
+        function captureVueError(){
             let origal = Vue.config.errorHandler || shared.noop;
             return function(err,vm,info){
-                report.send({ 
+                report.send({
                     message: err.message,
                     stack: err.stack,
                     info,
@@ -35,7 +52,7 @@ export const install = function(Vue){
 
         }
         
-        Vue.config.errorHandler = overVueErrorHandler()
+        Vue.config.errorHandler = captureVueError()
         
 
         /**vue resource load error */
@@ -43,7 +60,7 @@ export const install = function(Vue){
             let matchs = [HTMLImageElement, HTMLLinkElement, HTMLScriptElement]
             return matchs.filter(proto => srcElement instanceof proto).length
         }
-        function getResourceError(err){
+        function captureResourceError(err){
             if(matchResourceError(err.srcElement)){
                 report.send({
                     message: err.srcElement.currentSrc,
@@ -53,69 +70,70 @@ export const install = function(Vue){
                 })
             }
         }
-        window.addEventListener('error', getResourceError, true)
+        window.addEventListener('error', captureResourceError, true)
         
 
         /**promise not catch error */
-        window.addEventListener('unhandledrejection', e => {
-            // report.send(e)
+        function capturePromiseError(e) {
             console.error('PROMISE', e)
             console.error(`UNHANDLED PROMISE REJECTION: ${e.reason}`);
-        }, false)
+        }
+        window.addEventListener('unhandledrejection', capturePromiseError, false)
 
 
         /**ajax interactive exception */
+        function captureAjaxError(){
+            function newXHR(){
+                let realXHR = new oldXMLHttpRequest()
+                    let eventName
+                    events.forEach(event => {
+                        realXHR.addEventListener(event, function(){
+                            eventName = `ajax${event[0].toUpperCase() + event.slice(1)}`
+                            createCustomEvent(eventName)
+                        })
+                    })
+                
+                return realXHR
+            }
+            events.forEach(event => {
+                window.addEventListener(`ajax${event[0].toUpperCase() + event.slice(1)}`, e => {
+                    ajaxHandlerCb(event, {_reportEvent: true, e})
+                })
+            })
+            window.XMLHttpRequest = newXHR
+             
+            const ajaxReportHandler = {
+                open(e){
+                    // console.log('open', e)
+                    // report.send(err)
+                },
+                send(e){
+                    // console.log('send', e)
+                },
+                loadstart(e){
+                    // console.log('loadstart', e)
+                },
+                loadend(e){
+                    // console.log('loadend', e)
+                },
+                load(e){
+                    // console.log('load', e)
+                },
+                abort(e){
+                    // console.log('abort', e)
+                },
+            }
+            function ajaxHandlerCb(eventName, e) {
+                ajaxReportHandler[eventName](e)
+            }
+        }
         function createCustomEvent(eventName){
             let event = new CustomEvent(eventName, { detail: this })
             window.dispatchEvent(event)
         }
         
         
-        function newXHR(){
-            let realXHR = new oldXMLHttpRequest()
-                let eventName
-                events.forEach(event => {
-                    realXHR.addEventListener(event, function(){
-                        eventName = `ajax${event[0].toUpperCase() + event.slice(1)}`
-                        createCustomEvent(eventName)
-                    })
-                })
-            
-            return realXHR
-        }
-        events.forEach(event => {
-            window.addEventListener(`ajax${event[0].toUpperCase() + event.slice(1)}`, e => {
-                ajaxHandlerCb(event, {_reportEvent: true, e})
-            })
-        })
-        window.XMLHttpRequest = newXHR
-         
-        const ajaxReportHandler = {
-            open(e){
-                // console.log('open', e)
-                // report.send(err)
-            },
-            send(e){
-                // console.log('send', e)
-            },
-            loadstart(e){
-                // console.log('loadstart', e)
-            },
-            loadend(e){
-                // console.log('loadend', e)
-            },
-            load(e){
-                // console.log('load', e)
-            },
-            abort(e){
-                // console.log('abort', e)
-            },
-        }
-        function ajaxHandlerCb(eventName, e) {
-            // eventName = eventName.slice(4).toLowerCase()
-            // console.log('自定义类型？',e)
-            ajaxReportHandler[eventName](e)
-        }
+        
 
 
         /**项目重新刷新，是否又会重新引入一次？再次绑定再次初始化？ */
@@ -137,6 +155,7 @@ export const install = function(Vue){
         function removePromiseEvent(){
             window.removeEventListener('unhandledrejection', () => shared.noop)
         }
-        
+
+export const Report = captureHandler
     
-}
+// }
